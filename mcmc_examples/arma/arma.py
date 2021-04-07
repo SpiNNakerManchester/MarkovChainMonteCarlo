25396d79258a47270f93403bbf4dbb0527ab86bd
Andrew Gait <andrew.gait@manchester.ac.uk>
2021-04-07 14:39:43 +0100

    More sensible n_boards default value

--- mcmc_examples/arma/arma.py
+++ mcmc_examples/arma/arma.py
@@ -82,15 +82,9 @@ model = ARMAFloatModel(parameters, jump_scale)  # note: this sets both True
 #    alpha_jump_scale, alpha_min, alpha_max, beta_jump_scale, beta_min,
 #    beta_max)
 
-# Uncomment below for a spinn-5 run
-# samples = mcmc_framework.run_mcmc(
-#    model, data_points, n_samples, burn_in=5000, thinning=50,
-#    degrees_of_freedom=6.0, seed=seed, n_boards=n_boards)
-
-# Uncomment below for a spinn-3 run
 samples = mcmc_framework.run_mcmc(
-    model, data_points, n_samples, burn_in=5000, thinning=50,
-    degrees_of_freedom=6.0, seed=seed, n_boards=1)
+   model, data_points, n_samples, burn_in=5000, thinning=50,
+   degrees_of_freedom=6.0, seed=seed, n_boards=n_boards)
 
 # print('samples: ', samples)
 
