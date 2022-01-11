f30dd8da1dab28248943520a59e5f476b5dc14df
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2022-01-11 08:22:35 +0000

    flake8

--- mcmc/mcmc_root_finder_vertex.py
+++ mcmc/mcmc_root_finder_vertex.py
@@ -16,7 +16,6 @@
 from pacman.model.graphs.machine import MachineVertex
 from pacman.model.resources import ResourceContainer, ConstantSDRAM
 from spinn_utilities.overrides import overrides
-from pacman.executor.injection_decorator import inject_items
 
 from spinn_front_end_common.abstract_models.abstract_has_associated_binary \
     import AbstractHasAssociatedBinary
